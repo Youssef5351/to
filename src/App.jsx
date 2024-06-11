9e6d0ca283875f75243883f758eaa02865db11b3
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-06-12 00:46:32 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,48 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./Components/Navbar/Navbar";
+// import LandingPage from "./Components/LandingPage/LandingPage";
+// import Desc from "./Components/Desc/Desc";
+// import CrispChat from "./Components/Ui/CrispChat";
+// import CrispProvider from "./Components/Ui/CrispProvider";
+// import Master from "./Components/Master/Master";
+// import Infinite from "./Components/Infinite/Infinite";
+// import SignUp from "./Components/SignUp/SignUp";
+
+// import Location from "./Components/Location/Location";
+
+// import Footer from "./Components/Footer/Footer";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <div>
+//                 <CrispChat />
+//                 <CrispProvider />
+//                 <LandingPage />
+//                 <Master />
+//                 <Infinite />
+//                 <Desc />
+//                 <Location />
+//                 <Footer/>
+//               </div>
+//             }
+//           />
+//         <Route path="/signup" element={<SignUp />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
@@ -8,10 +53,8 @@ import CrispProvider from "./Components/Ui/CrispProvider";
 import Master from "./Components/Master/Master";
 import Infinite from "./Components/Infinite/Infinite";
 import SignUp from "./Components/SignUp/SignUp";
+import Location from "./Components/Location/Locaction";
 
-import Location from "./Components/Location/Location";
-
-import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
@@ -28,13 +71,13 @@ const App = () => {
                 <LandingPage />
                 <Master />
                 <Infinite />
+                {/* <ScrollSection /> */}
                 <Desc />
                 <Location />
-                <Footer/>
               </div>
             }
           />
-        <Route path="/signup" element={<SignUp />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </div>
     </Router>
