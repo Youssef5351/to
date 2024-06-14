63cd8cd81d60c12f59ea14c27f4d804a3e827f1c
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-06-15 01:07:37 +0300

    Update SignUp.jsx

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -81,11 +81,11 @@ const SignUp = () => {
 
     return (
         <div className="sign-up">
-                        {error && (
-                            <div className={`error ${error ? 'show' : ''}`}>
-                                <FontAwesomeIcon icon={faXmark} /> {error}
-                            </div>
-                            )}
+            {error && (
+                <div className={`error ${error ? 'show' : ''}`}>
+                    <FontAwesomeIcon icon={faXmark} /> {error}
+                </div>
+            )}
             <form onSubmit={handleSubmit}>
                 <section className="section-bg-white">
                     <div className="grid-container">
