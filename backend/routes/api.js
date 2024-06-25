30bf17bf06435c91818ff420d77a720a0bdf5e78
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2024-06-25 08:29:57 +0300

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,27 +1,54 @@
-// routes/api.js
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
 
 router.post('/api/signup', async (req, res) => {
-  const { first_name,last_name, email, password, confirmation_password} = req.body;
+  const { first_name, last_name, email, password, confirmation_password } = req.body;
 
-  // Check if the user already exists
-  const existingUser = await User.findOne({ email: email });
+  const existingUser = await User.findOne({ email });
   if (existingUser) {
     return res.status(400).json({ message: 'User already registered' });
   }
 
-  // Create a new user
-  const newUser = new User({ first_name,last_name, email, password, confirmation_password});
+  const newUser = new User({ first_name, last_name, email, password });
   await newUser.save();
 
   res.status(201).json({ message: 'User registered successfully' });
 });
 
+router.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid Password' });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ message: 'Login successful', token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
 router.get('/api/users', async (req, res) => {
-  const users = await User.find();
-  res.status(200).json(users);
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
